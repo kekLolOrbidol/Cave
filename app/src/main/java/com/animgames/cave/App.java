782a6d507c4aa65c8499ad43279b607778b333bb
apostolus <neketapostolov@gmail.com>
2020-11-27 13:49:33 +0200

    one signal

--- app/src/main/java/com/animgames/cave/App.java
+++ app/src/main/java/com/animgames/cave/App.java
@@ -2,6 +2,22 @@ package com.animgames.cave;
 
 import android.app.Application;
 
+import com.onesignal.OneSignal;
+
 public class App extends Application {
     static final String AF_DEV_KEY = "2KTn2FFy4YwGYoQEgmsDCg";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
 }
